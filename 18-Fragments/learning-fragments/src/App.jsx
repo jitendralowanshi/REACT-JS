dd46af47e1bd2d1e7a9f8b70bd49d91416bbb048
jitendra Lowanshi <jitendralowanshi3@gmail.com>
2025-06-01 22:41:17 +0530

    improve

--- 18-Fragments/learning-fragments/src/App.jsx
+++ 18-Fragments/learning-fragments/src/App.jsx
@@ -1,17 +1,28 @@
 import "./App.css";
 import FoodItems from "./components/FoodItems";
 import ErrorMsg from "./components/ErrorMsg";
+import Container from "./components/Container";
 
 function App() {
   // let foodItems = [];
-  let foodItems = ["banana", "Green Vegetable", "Roti", "Salad", "Milk"];
+  let foodItems = ["Banana", "Green Vegetable", "Roti", "Salad", "Milk"];
 
   return (
     <>
-      {/* 21 passing data via props */}
-      <h1>Healthy Food</h1>
-      <ErrorMsg items={foodItems}></ErrorMsg>
-      <FoodItems items={foodItems}></FoodItems>
+      <Container>
+        {/* 21 passing data via props */}
+        <h1 className="heading">Healthy Food</h1>
+        <ErrorMsg items={foodItems}></ErrorMsg>
+        <FoodItems items={foodItems}></FoodItems>
+      </Container>
+      <Container>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis nam,
+          placeat explicabo voluptatum corporis recusandae voluptatibus
+          voluptatem vero. Omnis corrupti similique ipsa dignissimos natus
+          voluptatum quos quidem ducimus harum aperiam?
+        </p>
+      </Container>
     </>
   );
 }
